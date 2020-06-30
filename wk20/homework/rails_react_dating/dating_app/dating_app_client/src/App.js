a89c89ca98f186b1f299ad571b05dea76589ac08
Katie <katie@katies-air.lan>
2020-06-30 14:41:33 -0400

    adding week 20 homework

---
+++ wk20/homework/rails_react_dating/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Daters from './components/Hello.js';
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+     <Daters />
+    </div>
+  );
+}
+}
+export default App;
